a9ee19feb714f7027ec2836306db21826ebfdc5b
mohammadhossein-karimi <m22505961@gmail.com>
2022-11-22 09:45:53 +0330

    create product

---
+++ js/products.js
@@ -0,0 +1,492 @@
+[
+  {
+    "id": 11210,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "12,450,00",
+    "Exist": "yes",
+    "source": "images/11210.jpg"
+  },
+  {
+    "id": 11350,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 20,
+    "price": "8,652,00",
+    "Exist": "yes",
+    "source": "images/11350.jpg"
+  },
+  {
+    "id": 11215,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11215.jpg"
+  },
+  {
+    "id": 11260,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 10,
+    "price": "6,985,000",
+    "Exist": "yes",
+    "source": "images/11260.jpg"
+  },
+  {
+    "id": 11270,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "4,725,000",
+    "Exist": "yes",
+    "source": "images/11270.jpg"
+  },
+  {
+    "id": 11355,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "2,995,000",
+    "Exist": "yes",
+    "source": "images/11355.jpg"
+  },
+  {
+    "id": 11365,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11365.jpg"
+  },
+  {
+    "id": 11305,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 40,
+    "price": "7,684,000",
+    "Exist": "yes",
+    "source": "images/11305.jpg"
+  },
+  {
+    "id": 11310,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 20,
+    "price": "6,354,000",
+    "Exist": "yes",
+    "source": "images/11310.jpg"
+  },
+  {
+    "id": 11435,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 0,
+    "price": "9,248,000",
+    "Exist": "yes",
+    "source": "images/11435.jpg"
+  },
+  {
+    "id": 11440,
+    "gender": "آقایان",
+    "Grouping": "کت مردانه",
+    "subgroup": "کت مردانه",
+    "discount percent": 30,
+    "price": "8,475,000",
+    "Exist": "yes",
+    "source": "images/11440.jpg"
+  },
+  {
+    "id": 11360,
+    "gender": "آقایان",
+    "Grouping": "کت پسرانه",
+    "subgroup": "کت پسرانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11360.jpg"
+  },
+  {
+    "id": 11390,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف اداری و فولدر",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11390.jpg"
+  },
+  {
+    "id": 11395,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف اداری و فولدر",
+    "discount percent": 0,
+    "price": "1,100,00",
+    "Exist": "yes",
+    "source": "images/11395.jpg"
+  },
+  {
+    "id": 11385,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف کراس بادی",
+    "discount percent": 0,
+    "price": "900,000",
+    "Exist": "yes",
+    "source": "images/11385.jpg"
+  },
+  {
+    "id": 11200,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف اداری و فولدر",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11200.jpg"
+  },
+  {
+    "id": 11225,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف اداری و فولدر",
+    "discount percent": 0,
+    "price": "2,600,000",
+    "Exist": "yes",
+    "source": "images/11225.jpg"
+  },
+  {
+    "id": 11230,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کیف اداری و فولدر",
+    "discount percent": 0,
+    "price": "2,420,000",
+    "Exist": "yes",
+    "source": "images/11230.jpg"
+  },
+  {
+    "id": 11265,
+    "gender": "آقایان",
+    "Grouping": "کیف و کوله",
+    "subgroup": "کوله پشتی",
+    "discount percent": 0,
+    "price": "1,598,000",
+    "Exist": "yes",
+    "source": "images/11265.jpg"
+  },
+  {
+    "id": 11235,
+    "gender": "آقایان",
+    "Grouping": "کفش",
+    "subgroup": "کفش کلاسیک",
+    "discount percent": 10,
+    "price": "2,478,000",
+    "Exist": "yes",
+    "source": "images/11235.jpg"
+  },
+  {
+    "id": 11315,
+    "gender": "آقایان",
+    "Grouping": "کفش",
+    "subgroup": "کفش اسپورت",
+    "discount percent": 15,
+    "price": "3,650,000",
+    "Exist": "yes",
+    "source": "images/11315.jpg"
+  },
+  {
+    "id": 11320,
+    "gender": "آقایان",
+    "Grouping": "کفش",
+    "subgroup": "کفش اسپورت",
+    "discount percent": 0,
+    "price": "3,145,000",
+    "Exist": "yes",
+    "source": "images/11320.jpg"
+  },
+  {
+    "id": 11240,
+    "gender": "آقایان",
+    "Grouping": "کفش",
+    "subgroup": "کفش کلاسیک",
+    "discount percent": 0,
+    "price": "2,870,000",
+    "Exist": "yes",
+    "source": "images/11240.jpg"
+  },
+  {
+    "id": 11285,
+    "gender": "آقایان",
+    "Grouping": "کلاه و دستکش مردانه",
+    "subgroup": "دستکش مردانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11285.jpg"
+  },
+  {
+    "id": 11290,
+    "gender": "آقایان",
+    "Grouping": "کلاه و دستکش مردانه",
+    "subgroup": " دستکش مردانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11290.jpg"
+  },
+  {
+    "id": 11205,
+    "gender": "خانم ها",
+    "Grouping": "کت زنانه",
+    "subgroup": "کت زنانه",
+    "discount percent": 10,
+    "price": "6,500,000",
+    "Exist": "yes",
+    "source": "images/11205.jpg"
+  },
+  {
+    "id": 11220,
+    "gender": "خانم ها",
+    "Grouping": "کت زنانه",
+    "subgroup": "کت زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11220.jpg"
+  },
+  {
+    "id": 11400,
+    "gender": "خانم ها",
+    "Grouping": "کت زنانه",
+    "subgroup": "کت زنانه",
+    "discount percent": 15,
+    "price": "3,658,00",
+    "Exist": "yes",
+    "source": "images/11400.jpg"
+  },
+  {
+    "id": 11405,
+    "gender": "خانم ها",
+    "Grouping": "کت زنانه",
+    "subgroup": "کت زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11405.jpg"
+  },
+  {
+    "id": 11330,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "کفش اسپورت زنانه",
+    "discount percent": 35,
+    "price": "2,220,000",
+    "Exist": "yes",
+    "source": "images/11330.jpg"
+  },
+  {
+    "id": 11335,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "کفش اسپورت زنانه",
+    "discount percent": 10,
+    "price": "3,614,000",
+    "Exist": "yes",
+    "source": "images/11335.jpg"
+  },
+  {
+    "id": 11340,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "بوت و نیم بوت زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11340.jpg"
+  },
+  {
+    "id": 11325,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "کفش مجلسی",
+    "discount percent": 20,
+    "price": "3,750,000",
+    "Exist": "yes",
+    "source": "images/11325.jpg"
+  },
+  {
+    "id": 11345,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "بوت و نیم بوت زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11345.jpg"
+  },
+  {
+    "id": 11245,
+    "gender": "خانم ها",
+    "Grouping": "کفش زنانه",
+    "subgroup": "کفش مجلسی",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11245.jpg"
+  },
+  {
+    "id": 11250,
+    "gender": "خانم ها",
+    "Grouping": "کمربند زنانه",
+    "subgroup": "کمربند زنانه",
+    "discount percent": 0,
+    "price": "1,114,000",
+    "Exist": "yes",
+    "source": "images/11250.jpg"
+  },
+  {
+    "id": 11255,
+    "gender": "خانم ها",
+    "Grouping": "کلاه و دستکش زنانه",
+    "subgroup": "دستکش زنانه",
+    "discount percent": 0,
+    "price": "874,000",
+    "Exist": "yes",
+    "source": "images/11255.jpg"
+  },
+  {
+    "id": 11275,
+    "gender": "خانم ها",
+    "Grouping": "کلاه و دستکش زنانه",
+    "subgroup": "دستکش زنانه",
+    "discount percent": 0,
+    "price": "958,000",
+    "Exist": "yes",
+    "source": "images/11275.jpg"
+  },
+  {
+    "id": 11280,
+    "gender": "خانم ها",
+    "Grouping": "ست زنانه",
+    "subgroup": "ست زنانه",
+    "discount percent": 10,
+    "price": "2,145,000",
+    "Exist": "yes",
+    "source": "images/11280.jpg"
+  },
+  {
+    "id": 11295,
+    "gender": "خانم ها",
+    "Grouping": "کت دخترانه",
+    "subgroup": "کت دخترانه",
+    "discount percent": 0,
+    "price": "3,721,000",
+    "Exist": "yes",
+    "source": "images/11295.jpg"
+  },
+  {
+    "id": 11300,
+    "gender": "خانم ها",
+    "Grouping": "کت دخترانه",
+    "subgroup": "کت دخترانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11300.jpg"
+  },
+  {
+    "id": 11370,
+    "gender": "خانم ها",
+    "Grouping": "کیف و کوله زنانه",
+    "subgroup": "کیف اداری و فولدر زنانه",
+    "discount percent": 0,
+    "price": "1,254,000",
+    "Exist": "yes",
+    "source": "images/11370.jpg"
+  },
+  {
+    "id": 11375,
+    "gender": "خانم ها",
+    "Grouping": "کیف و کوله زنانه",
+    "subgroup": "کیف اداری و فولدر زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11375.jpg"
+  },
+  {
+    "id": 11380,
+    "gender": "خانم ها",
+    "Grouping": "کیف پول زنانه",
+    "subgroup": "کیف پول زنانه",
+    "discount percent": 0,
+    "price": "2,340,000",
+    "Exist": "yes",
+    "source": "images/11380.jpg"
+  },
+  {
+    "id": 11415,
+    "gender": "خانم ها",
+    "Grouping": "کیف پول زنانه",
+    "subgroup": "کیف پول زنانه",
+    "discount percent": 0,
+    "price": "0",
+    "Exist": "no",
+    "source": "images/11415.jpg"
+  },
+  {
+    "id": 11410,
+    "gender": "لوازم جانبی",
+    "Grouping": "جلد دسته چک",
+    "subgroup": "جلد دسته چک",
+    "discount percent": 0,
+    "price": "1,000,000",
+    "Exist": "yes",
+    "source": "images/11410.jpg"
+  },
+  {
+    "id": 11420,
+    "gender": "لوازم جانبی",
+    "Grouping": "جا کلیدی",
+    "subgroup": "جا کلیدی",
+    "discount percent": 0,
+    "price": "59,000",
+    "Exist": "yes",
+    "source": "images/11420.jpg"
+  },
+  {
+    "id": 11425,
+    "gender": "لوازم جانبی",
+    "Grouping": "جا کارتی",
+    "subgroup": "جا کارتی",
+    "discount percent": 0,
+    "price": "241,000",
+    "Exist": "yes",
+    "source": "images/11425.jpg"
+  },
+  {
+    "id": 11430,
+    "gender": "لوازم جانبی",
+    "Grouping": "واکس",
+    "subgroup": "واکس",
+    "discount percent": 0,
+    "price": "180,000",
+    "Exist": "yes",
+    "source": "images/11430.jpg"
+  }
+ ]
